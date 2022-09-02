56e4a60313e866f941bd1f60b58009f32f7c557c
Gabriel Borges <gabriel.moura2009@hotmail.com>
2022-09-02 18:32:29 -0300

    chore: refactor

--- mente_sa-project/src/main.jsx
+++ mente_sa-project/src/main.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
-
-import Routers from './routes'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Routers />
+    <App />
   </React.StrictMode>
 )
