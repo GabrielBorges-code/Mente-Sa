23fcebc3cfb5888e62819e487a957b093f493fb2
Gabriel Borges <gabriel.moura2009@hotmail.com>
2022-09-02 19:00:23 -0300

    chore: restructuring

--- mente_sa-project/src/routes/index.jsx
+++ mente_sa-project/src/routes/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
+import Views from '../views/home'
+
 export default function Routers() {
     return (
 
         <BrowserRouter>
             <Routes>
-                <Route path='/' /> 
-                
+                <Route path='/' element={<Views/>}/> 
+                {/* <Route path="*" element={<Navigate to="/"/>}/> */}
                 
             </Routes>
         </BrowserRouter>
