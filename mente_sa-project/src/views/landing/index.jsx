1b2d2aadec5085e2bd24beb08392853f3990d850
Douglas Mendes <lixao_9@hotmail.com>
2022-09-02 21:03:19 -0300

    feat: created landing page

--- mente_sa-project/src/views/landing/index.jsx
+++ mente_sa-project/src/views/landing/index.jsx
@@ -1,6 +1,8 @@
 import * as React from 'react'
 import styles from './styles.css'
 import Logo from '../../assets/logo.svg'
+//icons
+import {BsFillCameraVideoFill, BsFillCalendar2EventFill} from 'react-icons/bs'
 
 export default function Landing() {
     
@@ -8,14 +10,16 @@ export default function Landing() {
         <>
             <div className='container-md'>
                 <div>
-                <div className='row align-items logo-div'>
-                    <img className='logo' src={Logo} alt="" />
+                <div className='row align-items d-md-flex justify-content-md-center logo-div'>
+                    <img className='img-fluid logo' src={Logo} alt="" />
+
+                    <div className='d-grid gap-2 d-md-flex justify-content-md-center'>
+                    <button className='btn btn-lg btn-primary default'><span><BsFillCalendar2EventFill/> </span>Agendar Sessão</button>
+                    <button className='btn btn-lg btn-primary dark '> <span><BsFillCameraVideoFill/></span> Ofertar Sessão </button></div>
+                    
                 </div>
-                <div>
-                    <div><button type='button' class='btn btn-primary default'> teste</button></div>
-                    <div><button type='button' class='btn  btn-primary dark '> teste</button></div>
                 </div>
-                </div>
+                
             </div>
         </>
        
