82c029581defbbee7a18a3f7d13a8423d4de0fb4
Douglas Mendes <lixao_9@hotmail.com>
2022-09-08 13:29:51 -0300

    feat: inserting new elements, initiating to create the edit profile and forms

--- mente_sa-project/src/views/professional/profile/index.jsx
+++ mente_sa-project/src/views/professional/profile/index.jsx
@@ -30,20 +30,17 @@ function ProfileProfessional() {
         <section className="h-100 gradient-custom-2">
             
                   <div className="">
-                    <div className="text-white d-flex flex-row justify-content-center" style={{backgroundColor: "#5086C1", height: "200px"}} >
-                      <div className="ms-4 mt-5 d-flex flex-column " style={{width: "150px", zIndex: 1}}>
+                    <div className="text-white d-flex flex-row justify-content-center " style={{backgroundColor: "#5086C1", height: "200px"}} >
+                      <div className="ms-4 mt-5 d-flex flex-column  " style={{width: "150px", zIndex: 1}}>
                         <img src={Image}
-                          alt="Generic placeholder image" className="img-fluid img-thumbnail mt-5 mb-3"
+                          alt="Generic placeholder image" className="img-fluid img-thumbnail mt-5  mb-3"
                           style={{width: "150px", zIndex: 1}}/>
-                        <button type="button" className="btn btn-sm btn-primary " 
-                          style={{zIndex: 1}}>
-                          <HiOutlinePencilAlt />
-                        </button>
+                          
                       </div>
-                      <div className="ms-3" style={{marginTop: "100px"}}>
-                        <h5>Dra Amanda Piovanni</h5>
-                        <p>Psicologo Comportamental</p>
-                        <p>CRP 06/156944</p>
+                      <div className="ms-4" style={{marginTop: "100px"}}>
+                            <p className="  fs-5 fw-bold  text-center">Dra Amanda Piovanni</p>
+                            <p className="text-wrap text-center lh-1 ">Psicologo Comportamental</p>
+                            <p className="text-wrap text-center lh-1 ">CRP 06/156944</p>
                       </div>
                     </div>
                     
@@ -52,11 +49,11 @@ function ProfileProfessional() {
                       <div className="d-flex justify-content-end text-center py-1">
                         <div>
                           
-                          <button className="btn btn-primary"><i><HiUserCircle/></i> Meu Perfil</button>
+                          <button className="btn btn-primary rounded-pill"><i><HiUserCircle/></i> Meu Perfil</button>
                         </div>
                         <div className="px-3">
                           
-                          <button className="btn btn-primary"><i><HiChatAlt/></i> Atendimento</button>
+                          <button className="btn btn-primary rounded-pill"><i><HiChatAlt/></i> Atendimento</button>
                         </div>
                         
                       </div>
@@ -67,19 +64,14 @@ function ProfileProfessional() {
           </section>
           
         
-        <Container className="">
-        
-          
-        </Container>
+       
         
         <Container className={`${styles.min_height} bg-light card`}>
 
           <section>
             <input/>
           </section>
-        
-  
-          
+   
         </Container>
   
         <Footer />
