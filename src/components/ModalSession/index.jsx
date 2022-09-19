b6b9acbb634e968ab6a0dad7f454775cfa94111f
mariana <marianaleitemelo4@gmail.com>
2022-09-19 15:06:18 -0300

    feat: creating ModalAnamnese and ModalShowProfile

--- src/components/ModalSession/index.jsx
+++ src/components/ModalSession/index.jsx
@@ -1,41 +1,23 @@
-import { useContext } from "react";
-
 import Button from "react-bootstrap/Button";
 import Modal from 'react-bootstrap/Modal';
-import Form from "react-bootstrap/Form";
 
 
 import { AuthGoogleContext, AuthGoogleProvider } from "../../contexts/authGoogle";
-import ModalShowProfile from "./ModalShowProfile";
+//import ModalShowProfile from "./ModalShowProfile";
 import ModalCadastroAnamnese from "./ModalCadastroAnamnese";
 
 function ModalSession (props) {
     //const {role1} = useContext(AuthGoogleContext);
 
     let modal = (
-        <ModalCadastroAnamnese></ModalCadastroAnamnese>
+        <ModalCadastroAnamnese {...props}></ModalCadastroAnamnese>
     );
+
+     console.log("PROPS: " + props.show);
 
     return (
         <>
-            <Modal
-                {...props}
-                size="lg"
-                aria-labelledby="contained-modal-title-vcenter"
-                centered
-                >
-                <Modal.Header closeButton>
-                    <Modal.Title id="contained-modal-title-vcenter">
-                    Modal heading
-                    </Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    {modal}
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button onClick={props.onHide}>Close</Button>
-                </Modal.Footer>
-            </Modal>
+            {modal}
         </>
     );
 }
