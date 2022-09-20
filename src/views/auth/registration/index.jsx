321f2a3e2fed1a83c5ecc286682cefa64f7e8779
Douglas Mendes <lixao_9@hotmail.com>
2022-09-20 08:47:09 -0300

    feat: inserting function to sendToLogin inside registration button labelled login

--- src/views/auth/registration/index.jsx
+++ src/views/auth/registration/index.jsx
@@ -25,9 +25,9 @@ export default function Registration({edit}) {
         const [repeatPassword, setRepeatPassword] = useState("")
 
 
-        async function handleAddUserRole(){
+        async function sendToLogin(){
             
-            return; 
+            navigate ('/login')
         }
 
         async function registerRole(uid) {
@@ -48,23 +48,15 @@ export default function Registration({edit}) {
                 console.log('oiadfs')
                 toast.error('As senhas não estão iguais')
             }else{
-
-                // const register = await createWithEmailPasswordGoogle(email, password)
-                
                 createUserWithEmailAndPassword(auth, email, password)
                 .then(async (userCredential) => {
                     const user = userCredential.user;
                     registerRole(user.uid)
                     toast.success('Usuário registrado com sucesso')
                     navigate ('/login')
-                    
-                     
                 })
                 .catch((error) => {
                     const errorCode = error.code;
-                // const errorMessage = error.message;
-                
-                
                 let errorMessage = VerifyErrorCode(errorCode);
                     if (errorMessage == null) {
                         errorMessage = error.message;
@@ -91,7 +83,7 @@ export default function Registration({edit}) {
                         <form onSubmit={handleRegistration}>
                         <div className="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
                             <p className="lead fw-normal mb-0 me-3">Já sou usuário </p>
-                            <button onClick={()=>{signInGoogle()}} type="button" className="btn btn-danger btn-floating mx-1">
+                            <button onClick={()=>{sendToLogin()}} type="button" className="btn btn-danger btn-floating mx-1">
                             <i>Login</i>
                             </button>
 
