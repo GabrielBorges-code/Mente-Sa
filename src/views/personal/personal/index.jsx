31f2adb43f434a32467c854fb68d425198126920
Gabriel Borges <gabriel.moura2009@hotmail.com>
2022-09-20 12:14:09 -0300

    feat: add news features in register

--- src/views/personal/personal/index.jsx
+++ src/views/personal/personal/index.jsx
@@ -7,64 +7,140 @@ import InputSelect from "../../../components/Input/inputSelect";
 
 import Footer from "../../../components/Footer";
 import Header from "../../../components/Header";
-import Stepper from "../../../components/OneStep";
+import Stepper from "../../../components/Stepper";
 
 import { useState } from "react";
 import styles from "./index.module.css";
 
+import { doc, setDoc, getDoc } from "firebase/firestore";
+import { db, auth } from "../../../services/firebase";
+import { useEffect } from "react";
+
 function Personal() {
   let navigate = useNavigate();
-  const [] = useState();
+  const [users, setUsers] = useState(
+    JSON.parse(sessionStorage.getItem("@AuthFirebase:user"))
+  );
+  const [name, setName] = useState(null);
 
-  function save() {
+  const [phone, setPhone] = useState(null);
+  const [dateBorn, setDateBorn] = useState(null);
+  const [civilState, setCivilState] = useState(null);
+  const [genre, setGenre] = useState(null);
+  const [street, setStreet] = useState(null);
+  const [numberHouse, setNumberHouse] = useState(null);
+  const [complement, setComplement] = useState(null);
+  const [district, setDistrict] = useState(null);
+  const [state, setState] = useState(null);
+  const [city, setCity] = useState(null);
+
+  async function handleSave(e) {
     //PREPARE TO SAVE
+
+    //TODO we have changing fiels: "data nascimento", "estado civil", "sexo"
+    const info = await setDoc(doc(db, "PatientePersonal", users.uid), {
+      name: name,
+      phone: phone,
+      dateBorn: dateBorn,
+      civilState: civilState,
+      genre: genre,
+      street: street,
+      numberHouse: numberHouse,
+      complement: complement,
+      district: district,
+      state: state,
+      city: city,
+    });
+
     navigate("/usuario/perfil");
   }
 
+  async function handleEdit() {
+    const docRef = doc(db, "PatientePersonal", users.uid);
+    const docSnap = await getDoc(docRef);
+    const {
+      city,
+      civilState,
+      complement,
+      dateBorn,
+      district,
+      genre,
+      name,
+      numberHouse,
+      phone,
+      state,
+      street      
+    } = docSnap.data();
+      setCity(city)
+      setCivilState(civilState)
+      setComplement(complement)
+      setDateBorn(dateBorn)
+      setDistrict(district)
+      setGenre(genre)
+      setName(name)
+      setNumberHouse(numberHouse)
+      setPhone(phone)
+      setState(state)
+      setStreet(street)  
+  }
+
+  useEffect(() => {
+    console.log(users);
+    handleEdit();
+  }, []);
+
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    event.preventDefault();
+
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+      return setValidated(true);
+    }
+    setValidated(true);
+    handleSave();
+  };
+
   return (
     <>
-      <Header message={"Olá paciente, queremos te conhecer!"} linkBack={"/login"} />
-{/*       
-      <Stepper
-        step={'0'}
+      <Header message={"Olá paciente, queremos te conhecer!"} linkBack={"/"} />
+      {/* steper */}
+      {/* <Stepper
+        step={"0"}
         icon={true}
         state1={"primary"}
-        
+        state2={"secondary"}
+        state3={"secondary"}
       /> */}
 
-
       <Container className={`${styles.content} bg-light card`}>
-        <Form>
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
           <Form.Group className="mb-5">
             <h3>Dados Pessoais</h3>
-            
+
             <div className="row">
-            <Input
+              <Input
                 type={"text"}
                 typeForm={"form-control"}
                 setClassCol={"col-sm"}
                 label={"Nome"}
-                required="required"
-
+                value={name}
+                setValue={setName}
+                required={true}
               />
             </div>
 
             <div className="row">
               <Input
-                type={"email"}
-                typeForm={"form-control"}
-                setClassCol={"col-sm-8"}
-                label={"Email"}
-                required="required"
-
-              />
-              <Input
                 type={"number"}
                 typeForm={"form-control"}
                 setClassCol={"col-sm"}
                 label={"Telefone"}
-                required="required"
-
+                value={phone}
+                setValue={setPhone}
+                required={true}
               />
             </div>
 
@@ -74,37 +150,49 @@ function Personal() {
                 typeForm={"form-control"}
                 setClassCol={"col-sm"}
                 label={"Data Nascimento"}
-                required="required"
-
+                value={dateBorn}
+                setValue={setDateBorn}
               />
-                
+
               <InputSelect
                 label={"Estado Civil"}
-                options={["Solteiro", "Casado", "Separado", "Divorciado", "Viúvo"]}
+                value={civilState}
+                setValue={setCivilState}
+                options={[
+                  "Solteiro",
+                  "Casado",
+                  "Separado",
+                  "Divorciado",
+                  "Viúvo",
+                ]}
               />
 
               <InputSelect
                 label={"Sexo"}
+                value={genre}
+                setValue={setGenre}
                 options={["Masculino", "Feminino"]}
               />
-              
             </div>
 
             <h3>Endereço</h3>
 
             <div className="row">
-
               <Input
                 type={"text"}
                 typeForm={"form-control"}
                 setClassCol={"col-sm-8"}
                 label={"Logradouro"}
+                value={street}
+                setValue={setStreet}
               />
               <Input
                 type={"number"}
                 typeForm={"form-control"}
                 setClassCol={"col-sm"}
                 label={"Número"}
+                value={numberHouse}
+                setValue={setNumberHouse}
               />
 
               <Input
@@ -112,8 +200,9 @@ function Personal() {
                 typeForm={"form-control"}
                 setClassCol={"col-sm"}
                 label={"Complemento"}
+                value={complement}
+                setValue={setComplement}
               />
-              
             </div>
 
             <div className="row">
@@ -122,14 +211,16 @@ function Personal() {
                 typeForm={"form-control"}
                 setClassCol={"col-sm"}
                 label={"Bairro"}
+                value={district}
+                setValue={setDistrict}
               />
               <Input
                 type={"text"}
                 typeForm={"form-control"}
                 setClassCol={"col-sm"}
                 label={"Estado"}
-                required="required"
-
+                value={state}
+                setValue={setState}
               />
 
               <Input
@@ -137,20 +228,17 @@ function Personal() {
                 typeForm={"form-control"}
                 setClassCol={"col-sm"}
                 label={"Cidade"}
-                required="required"
-
+                value={city}
+                setValue={setCity}
               />
-            
             </div>
-
           </Form.Group>
 
           <div className="row d-flex justify-content-center">
-            <button onClick={save} className="btn btn-lg col-md-3 btn-primary">
+            <button type="submit" className="btn btn-lg col-md-3 btn-primary">
               Avançar
             </button>
           </div>
-          
         </Form>
       </Container>
 
